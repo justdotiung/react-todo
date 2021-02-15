1ab9cba54e24e7d56eb685d7bf2b67343a961ff6
ung <2014brand@naver.com>
2021-02-16 08:23:22 +0900

    config 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+export const extends = ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'];
+export const parser = '@typescript-eslint/parser';
+export const plugins = ['@typescript-eslint', 'prettier'];
+export const rules = {};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const a = '';
+  return <div>gae</div>;
+}
+
+export default App;
